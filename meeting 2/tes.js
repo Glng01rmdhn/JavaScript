fdf23eef51d9e5ffa078a53ec551201ca54defa0
glng01rmdhn <galang2909@student.abudzar.sch.id>
2024-07-30 00:46:37 -0400

    javascript2

---
+++ meeting 2/tes.js
@@ -0,0 +1,22 @@
+
+// 1. Deklarasikan variabel tanpa memberikan nilai (undefined) dan cetak hasilnya. 
+var a;
+console.log(a);  
+
+// 2. buatlah 3 buah String tentang ( nama, umur dan tempattinggal ) dan Gabungkan ketiga string dan cetak hasilnya.
+var about = {
+    name:   "garma",
+    age:    "29",
+    home:   "puspitek"
+} 
+console.log(about);
+
+// 3. Inisialisasi variabel yang tidak memiliki nilai (null) dan cetak hasilnya. 
+var nu;
+console.log(null);
+
+// 4. Hitung luas segitiga, panjang alas = 5 dan tinggi = 7
+var alas =      5;
+var tinggi =    7;
+var rumus =     0.5;
+console.log(alas*tinggi*rumus);
