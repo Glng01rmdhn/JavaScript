3c33df16bff11d5c3515bb36b22490b5c1167f45
glng01rmdhn <galang2909@student.abudzar.sch.id>
2024-08-16 00:21:13 -0400

    new update

--- meeting 6/dot.js
+++ meeting 6/dot.js
@@ -9,7 +9,7 @@ console.log(fruit);
 console.log(fruit[0]);
 console.log(fruit[1]);
 console.log(fruit[2]);
-        // memunculkan sesuai urutan dari nol 
+        // memunculkan sesuai urutan (dari nol)
 
 
 
@@ -31,6 +31,7 @@ console.log(fruit);
 
 
 // Tugas!! 4
+//menggabungkan array
 const room1 = ['hamzah', 'hafs']// ini 
 const room2 = ['ashim', 'yaqub']// dan ini
                                 // di gabungkan
@@ -41,13 +42,13 @@ console.log(roomall);
 
 
 // Tugas!! 5
-
+//Mencari elemen array
 const student = ['galang', 'hektor', 'taqi']
-console.log(student.indexOf('galang'));
+console.log(student.indexOf('galang'));// jika galang maka nol, jika hektor 1, jika taqi 2
 
 
 // Tugas!! 6
-
+//Mengurutkan array
 function compareNumbers(a, b) {
     return a - b;
   }
@@ -57,6 +58,6 @@ array3.sort();
 console.log(array3.sort(compareNumbers)); // sort di gunakan untuk mengurutkan data dari angka yang terkeci
 
 // Tugas!! 7
-
+//menghitung jumlah data dalam array
 let array4 =  [34, 7, 23, 32, 5] // [34, 7, 23, 32, 5] in datanya
 console.log(array4.length); // hasil dari ada berapa data di atas
---
+++ meeting7/7.js
@@ -0,0 +1,71 @@
+//soal 1
+//object literal
+let obj = {
+    key: "value",
+};
+
+
+let me = {
+    name:"Garma",
+    age:"16",
+    addres: {
+        city:"tangsel",
+        country:"Rusian"
+    }
+}
+console.log(me.addres.country); //memunculkan country yang berada di dalam addres dan adres yang di dalam me
+
+//mengakses dan mengubah nilai properti
+
+console.log(me.age); //akses nilai menggunakan dot notation
+console.log(me ['addres']); // akses nilai menggunakan bracket notation
+
+obj.key = "car";
+console.log(obj['key']);
+
+obj ['key'] = "cars";
+console.log(obj['key']);
+
+//menghapus properti
+
+delete me.country;
+console.log(me.country);
+
+// soal 2
+
+const {Name, Age} = me;
+console.log(Name, Age);
+
+const {Name:UserName, Age:UserAge} = Person;
+console.log(UserName, UserAge);
+
+// Praktek 3
+
+
+class Book {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    getDetails() {
+        return `Title: ${this.title}, Author: ${this.author}`;
+    }
+
+    updateAuthor(newAuthor) {
+        this.author = newAuthor;
+    }
+}
+
+let myBook = new Book("JavaScript Essentials", "Jane Doe");
+console.log(myBook.getDetails());
+
+myBook.updateAuthor("Hektor");
+console.log(myBook.getDetails());
+
+
+
+
+
+
+
